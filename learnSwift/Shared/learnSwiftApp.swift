e8d8f7e1b13420f709084efaf799f8942337fa03
chanul94 <cksdnfwkd@naver.com>
2021-02-08 20:24:50 +0900

    Implement swift app

---
+++ learnSwift/Shared/learnSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnSwiftApp.swift
+//  Shared
+//
+//  Created by 박찬울 on 2021/02/05.
+//
+
+import SwiftUI
+
+@main
+struct learnSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

64d2d355ae212d6175fb36bea44bb284205ffc02
chanul94 <cksdnfwkd@naver.com>
2021-02-08 20:48:27 +0900

    Implement bountyList

---
+++ swiftList/BountyList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BountyList
+//
+//  Created by 박찬울 on 2021/02/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

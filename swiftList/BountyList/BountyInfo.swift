b0e79c6163994b7768917abc692128439cea0cf9
chanul94 <cksdnfwkd@naver.com>
2021-02-08 23:15:07 +0900

    Refactor bountyList

---
+++ swiftList/BountyList/BountyInfo.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+// model
+struct BountyInfo {
+    let name: String
+    let bounty: Int
+    
+    var image: UIImage? {
+        return UIImage(named: "\(name).jpg")
+    }
+    
+    init(name: String, bounty: Int) {
+        self.name = name
+        self.bounty = bounty
+    }
+}
--- swiftList/BountyList/BountyViewController.swift
+++ swiftList/BountyList/BountyViewController.swift
@@ -8,24 +8,16 @@
 import UIKit
 
 class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    
-    let bountyInfoList: [BountyInfo] = [
-        BountyInfo(name: "brook", bounty: 300000),
-        BountyInfo(name: "chopper", bounty: 50),
-        BountyInfo(name: "franky", bounty: 230000),
-        BountyInfo(name: "luffy", bounty: 300000),
-        BountyInfo(name: "nami", bounty: 1600000000),
-        BountyInfo(name: "robin", bounty: 800000),
-        BountyInfo(name: "sanji", bounty: 770000),
-        BountyInfo(name: "zoro", bounty: 120000000),
-    ]
+
+    let viewModel = BountyViewModel()
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // DetailViewController에 데이터 보내기
         if segue.identifier == "showDetail" {
             let vc = segue.destination as? DetailViewController
             if let index = sender as? Int {
-                vc?.bountyInfo = bountyInfoList[index]
+                let bountyInfo = viewModel.bountyInfo(at: index)
+                vc?.viewModel.update(model: bountyInfo)
             }
         }
     }
@@ -36,7 +28,7 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
     
     // UITableViewDataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return bountyInfoList.count
+        return viewModel.numOfBountyInfoList
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -44,10 +36,8 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
             return UITableViewCell()
         }
         
-        let bountyInfo = bountyInfoList[indexPath.row]
-        cell.imgView.image = bountyInfo.image
-        cell.nameLabel.text = bountyInfo.name
-        cell.bountyLabel.text = "\(bountyInfo.bounty)"
+        let bountyInfo = viewModel.bountyInfo(at: indexPath.row)
+        cell.update(info: bountyInfo)
         return cell
     }
     
@@ -62,20 +52,40 @@ class ListCell: UITableViewCell {
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var bountyLabel: UILabel!
+    
+    // viewController
+    func update(info: BountyInfo) {
+        imgView.image = info.image
+        nameLabel.text = info.name
+        bountyLabel.text = "\(info.bounty)"
+    }
 }
 
-
-// model
-struct BountyInfo {
-    let name: String
-    let bounty: Int
+// viewModel
+class BountyViewModel {
+    let bountyInfoList: [BountyInfo] = [
+        BountyInfo(name: "brook", bounty: 300000),
+        BountyInfo(name: "chopper", bounty: 50),
+        BountyInfo(name: "franky", bounty: 230000),
+        BountyInfo(name: "luffy", bounty: 300000),
+        BountyInfo(name: "nami", bounty: 1600000000),
+        BountyInfo(name: "robin", bounty: 800000),
+        BountyInfo(name: "sanji", bounty: 770000),
+        BountyInfo(name: "zoro", bounty: 120000000),
+    ]
     
-    var image: UIImage? {
-        return UIImage(named: "\(name).jpg")
+    var sortedList: [BountyInfo] {
+        let sortedList = bountyInfoList.sorted { prev, next in
+            return prev.bounty > next.bounty
+        }
+        return sortedList
     }
     
-    init(name: String, bounty: Int) {
-        self.name = name
-        self.bounty = bounty
+    var numOfBountyInfoList: Int {
+        return bountyInfoList.count
+    }
+    
+    func bountyInfo(at index: Int) -> BountyInfo {
+        return sortedList[index]
     }
 }

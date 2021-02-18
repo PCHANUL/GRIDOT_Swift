5c84cd6531d704459c8ae291903798efb69e165b
chanul94 <cksdnfwkd@naver.com>
2021-02-18 20:19:15 +0900

    Update history

--- nextNetflix/nextNetflix/HistoryViewController.swift
+++ nextNetflix/nextNetflix/HistoryViewController.swift
@@ -7,18 +7,18 @@
 
 import UIKit
 import Firebase
+import Kingfisher
 
 class HistoryViewController: UIViewController {
 
-    @IBOutlet weak var tableView: UITableView!
-    var searchTerm: [SearchTerm] = []
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var searchedMovies: [SearchedMovie] = []
     
     let db = Database.database().reference().child("searchHistory")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -29,39 +29,49 @@ class HistoryViewController: UIViewController {
             let data = try! JSONSerialization.data(withJSONObject: Array(searchHistory.values), options: [])
             
             let decoder = JSONDecoder()
-            let searchTerms = try! decoder.decode([SearchTerm].self, from: data)
-            self.searchTerm = searchTerms.sorted(by: { (term1, term2) in
-                return term1.timestamp > term2.timestamp
+            let searchedMovies = try! decoder.decode([SearchedMovie].self, from: data)
+            self.searchedMovies = searchedMovies.sorted(by: { (item1, item2) in
+                return item1.timestamp > item2.timestamp
             })
-            self.tableView.reloadData()
+            self.collectionView.reloadData()
         }
-
     }
 }
 
-extension HistoryViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return searchTerm.count
+extension HistoryViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return searchedMovies.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "HistoryCell", for: indexPath) as? HistoryCell else {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HistoryCell", for: indexPath) as? HistoryCell else {
             return HistoryCell()
         }
-        
-        cell.searchTerm.text = searchTerm[indexPath.row].term
+        let searchedMovie = searchedMovies[indexPath.item]
+        let url = URL(string: searchedMovie.thumbnailPath)!
+        cell.searchedMovie.kf.setImage(with: url)
+        cell.backgroundColor = .red
         return cell
     }
-    
-    
+}
+extension HistoryViewController: UICollectionViewDelegateFlowLayout {
+    // [x] collectionView 크기설정
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let margin: CGFloat = 8
+        let itemSpacing: CGFloat = 10
+        
+        let width = (collectionView.bounds.width - margin * 2 - itemSpacing * 2) / 3
+        let height = width * 10/7
+        return CGSize(width: width, height: height)
+    }
 }
 
-class HistoryCell: UITableViewCell {
-    @IBOutlet weak var searchTerm: UILabel!
+class HistoryCell: UICollectionViewCell {
+    @IBOutlet weak var searchedMovie: UIImageView!
 }
 
-struct SearchTerm: Codable {
-    let term: String
+struct SearchedMovie: Codable {
+    let previewURL: String
+    let thumbnailPath: String
     let timestamp: TimeInterval
 }

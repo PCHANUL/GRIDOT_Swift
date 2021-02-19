6096944040245277264d5592787c6f3a857421d6
chanul94 <cksdnfwkd@naver.com>
2021-02-20 00:34:32 +0900

    Implement canvas

---
+++ spriteMaker/spriteMaker/PreplayViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PreplayViewController.swift
+//  spriteMaker
+//
+//  Created by 박찬울 on 2021/02/19.
+//
+
+import UIKit
+
+class PreplayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

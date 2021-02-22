7e4118cf28353f9e701a0402c090b3a66d22c4ea
chanul94 <cksdnfwkd@naver.com>
2021-02-22 21:00:10 +0900

    Implement function check quadrant

--- spriteMaker/spriteMaker/ViewController.swift
+++ spriteMaker/spriteMaker/ViewController.swift
@@ -14,6 +14,9 @@ class Grid {
     
     init(numsOfPixels: Int) {
         self.createGrid(numsOfPixels: numsOfPixels)
+        girdArray[0][0] = 1
+        girdArray[1][1] = 1
+        girdArray[2][2] = 1
     }
     
     func isEmpty(targetPos: [Int]) -> Bool{
@@ -34,6 +37,7 @@ class Canvas: UIView {
     var positionOfCanvas: CGFloat
     var lengthOfOneSide: CGFloat
     var numsOfPixels: Int
+    var onePixelLength: CGFloat
     
     var isEmptyPixel: Bool
     var isTouchesMoved: Bool
@@ -48,10 +52,11 @@ class Canvas: UIView {
         self.positionOfCanvas = positionOfCanvas
         self.lengthOfOneSide = lengthOfOneSide
         self.numsOfPixels = numsOfPixels
+        self.onePixelLength = lengthOfOneSide / CGFloat(numsOfPixels)
+        
         self.isEmptyPixel = false
         self.isTouchesMoved = false
         self.isTouchesEnded = false
-        
         self.moveTouchPosition = CGPoint()
         self.initTouchPosition = CGPoint()
         
@@ -64,13 +69,29 @@ class Canvas: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // draw
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        
+        if isTouchesMoved {
+            if isTouchesEnded {
+                addDiagonalPixels(context: context)
+            } else {
+                drawTouchGuideLine(context: context)
+            }
+        }
+        drawSeletedPixels(context: context)
+        drawGridLine(context: context)
+    }
+    
+    // draw_method
     func drawGridLine(context: CGContext) {
         context.setStrokeColor(UIColor.white.cgColor)
         context.setLineWidth(0.5)
         
-        let term = lengthOfOneSide / CGFloat(numsOfPixels)
         for i in 1...Int(numsOfPixels - 1) {
-            let gridWidth = term * CGFloat(i)
+            let gridWidth = onePixelLength * CGFloat(i)
             context.move(to: CGPoint(x: gridWidth, y: 0))
             context.addLine(to: CGPoint(x: gridWidth, y: lengthOfOneSide))
             context.move(to: CGPoint(x: 0, y: gridWidth))
@@ -78,19 +99,19 @@ class Canvas: UIView {
         }
         context.strokePath()
     }
-    
     func drawSeletedPixels(context: CGContext) {
         // grid.gridArray를 참조하여 해당 칸을 색칠
+        context.setStrokeColor(UIColor.yellow.cgColor)
         context.setFillColor(UIColor.yellow.cgColor)
-        let widthOfPixel = Double(Int(lengthOfOneSide) / numsOfPixels)
+        let widthOfPixel = Double(onePixelLength)
         
         for i in 0..<numsOfPixels {
             for j in 0..<numsOfPixels {
                 if (grid.girdArray[i][j] == 1) {
                     let xIndex = Double(j)
                     let yIndex = Double(i)
-                    let x = xIndex * Double(widthOfPixel) + xIndex * 0.5
-                    let y = yIndex * Double(widthOfPixel) + yIndex * 0.5
+                    let x = xIndex * widthOfPixel
+                    let y = yIndex * widthOfPixel
                     let rectangle = CGRect(x: x, y: y, width: widthOfPixel, height: widthOfPixel)
                     
                     context.addRect(rectangle)
@@ -100,7 +121,6 @@ class Canvas: UIView {
         }
         context.strokePath()
     }
-    
     func drawTouchGuideLine(context: CGContext) {
         // 터치가 시작된 곳에서 부터 움직인 곳까지 경로를 표시
         context.setStrokeColor(UIColor.yellow.cgColor)
@@ -109,88 +129,91 @@ class Canvas: UIView {
         context.move(to: initTouchPosition)
         context.addLine(to: moveTouchPosition)
         context.strokePath()
+        
+        context.setFillColor(UIColor.yellow.cgColor)
+        context.addArc(center: moveTouchPosition, radius: onePixelLength / 2, startAngle: 0, endAngle: CGFloat(Double.pi * 2), clockwise: true)
+        context.fillPath()
     }
-    
-    func drawDiagonal(context: CGContext) {
-        // 가상의 상자를 만들고 비율에 따라서 중앙에 선을 긋습니다.
+    func getQuadrant(start: [Int], end: [Int]) -> [Int]{
+        // start를 기준으로한 사분면
+        return [(end[0] - start[0]).signum(), (end[1] - start[1]).signum()]
+    }
+    func addDiagonalPixels(context: CGContext) {
+        // 가상의 상자를 만들고 비율에 따라서 중앙에 선을 긋는다
         let startPoint = transPosition(point: initTouchPosition)
         let endPoint = transPosition(point: moveTouchPosition)
+        let quadrant = getQuadrant(start: startPoint, end: endPoint)
         
         print("--> start: ", startPoint)
         print("--> end: ", endPoint)
+        print(quadrant)
         
+        // 긴 변을 짧은 변으로 나눈 몫이 하나의 계단이 된다
+        let yLength = abs(startPoint[1] - endPoint[1]) + 1
+        let xLength = abs(startPoint[0] - endPoint[0]) + 1
+        let stairsLength = max(xLength, yLength) / min(xLength, yLength)
         
-        
-        for i in startPoint[1]...endPoint[1] {
-            for j in startPoint[0]...endPoint[0] {
-                grid.girdArray[i][j] = 1
+        for j in 0..<yLength {
+            for i in 0..<stairsLength {
+                let x = startPoint[0] + (i + j * stairsLength) * quadrant[0]
+                let y = startPoint[1] + (j) * quadrant[1]
+                print(x, y)
+                grid.girdArray[y][x] = 1
             }
         }
+        
         isTouchesEnded = false
+        isTouchesMoved = false
         context.strokePath()
     }
     
-    func transPosition(point: CGPoint) -> [Int]{
-        let pixelLength = Int(lengthOfOneSide) / numsOfPixels
-        let x = Int(point.x) / pixelLength
-        let y = Int(point.y) / pixelLength
-        return [x == 16 ? 15 : x, y == 16 ? 15 : y]
-    }
     
-    func findTouchPosition(touches: Set<UITouch>) -> CGPoint {
-        guard var point = touches.first?.location(in: nil) else { return CGPoint() }
-        point.x -= 20
-        point.y = point.y - positionOfCanvas
-        return point
-    }
-    
-    
-    
-    override func draw(_ rect: CGRect) {
-        super.draw(rect)
-        guard let context = UIGraphicsGetCurrentContext() else { return }
-        
-        drawGridLine(context: context)
-        if isTouchesMoved {
-            drawTouchGuideLine(context: context)
-        } else if isTouchesEnded {
-            // 터치 가이드 라인에 따라서 픽셀을 선택
-            drawDiagonal(context: context)
-        }
-        drawSeletedPixels(context: context)
-        
-    }
-    
-    func selectPixel(pixelPosition: [Int]) {
-        isEmptyPixel = grid.isEmpty(targetPos: pixelPosition)
-        grid.updateGrid(targetPos: pixelPosition, isEmptyPixel: isEmptyPixel)
-    }
-    
+    // touch
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // [] 더블클릭시 도형툴 활성화
+        
         let position = findTouchPosition(touches: touches)
         let pixelPosition = transPosition(point: position)
-        initTouchPosition = position
+        
+        let halfPixel = onePixelLength / 2
+        let initPositionX = CGFloat(pixelPosition[0]) * onePixelLength + halfPixel
+        let initPositionY = CGFloat(pixelPosition[1]) * onePixelLength + halfPixel
+        
+        initTouchPosition = CGPoint(x: initPositionX, y: initPositionY)
         moveTouchPosition = position
         
         selectPixel(pixelPosition: pixelPosition)
         setNeedsDisplay()
     }
-    
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let movePosition = findTouchPosition(touches: touches)
+        moveTouchPosition = CGPoint(x: movePosition.x - 20, y: movePosition.y - 20)
         isTouchesMoved = true
-        moveTouchPosition = findTouchPosition(touches: touches)
         setNeedsDisplay()
     }
-    
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        resetTouchStatus()
-        isTouchesEnded = true
+        isEmptyPixel = false
+        if isTouchesMoved {
+            isTouchesEnded = true
+        }
         setNeedsDisplay()
     }
     
-    func resetTouchStatus() {
-        isEmptyPixel = false
-        isTouchesMoved = false
+    // touch_method
+    func findTouchPosition(touches: Set<UITouch>) -> CGPoint {
+        guard var point = touches.first?.location(in: nil) else { return CGPoint() }
+        point.x -= 22
+        point.y = point.y - positionOfCanvas - 5
+        return point
+    }
+    func transPosition(point: CGPoint) -> [Int]{
+        let x = Int(point.x / onePixelLength)
+        let y = Int(point.y / onePixelLength)
+        return [x == 16 ? 15 : x, y == 16 ? 15 : y]
+    }
+    func selectPixel(pixelPosition: [Int]) {
+        isEmptyPixel = grid.isEmpty(targetPos: pixelPosition)
+        grid.updateGrid(targetPos: pixelPosition, isEmptyPixel: isEmptyPixel)
     }
 }
 
@@ -198,6 +221,7 @@ class ViewController: UIViewController {
         
     @IBOutlet weak var canvasView: UIView!
     @IBOutlet var viewController: UIView!
+    @IBOutlet weak var preview: UIImageView!
     
     override func viewSafeAreaInsetsDidChange() {
         // 캔버스의 위치와 크기는 canvasView와 같다
@@ -210,6 +234,14 @@ class ViewController: UIViewController {
         canvasView.addSubview(canvas)
         canvas.backgroundColor = .systemGray2
         canvas.frame = CGRect(x: 0, y: 0, width: lengthOfOneSide, height: lengthOfOneSide)
+        
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        let image = renderer.image { context in
+            canvas.drawSeletedPixels(context: context.cgContext)
+        }
+        preview.image = image
+        print(image)
+        
     }
     
     override func viewDidLoad() {

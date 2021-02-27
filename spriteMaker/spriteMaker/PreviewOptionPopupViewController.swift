cfd17d7a578d6be85033da97836ea34f5fe7c7d5
chanul94 <cksdnfwkd@naver.com>
2021-02-27 23:43:20 +0900

    Fix popup pan scroll

--- spriteMaker/spriteMaker/PreviewOptionPopupViewController.swift
+++ spriteMaker/spriteMaker/PreviewOptionPopupViewController.swift
@@ -20,8 +20,6 @@ class PreviewOptionPopupViewController: UIViewController {
         
         let bottomSafeArea = UIApplication.shared.windows[0].safeAreaInsets.top
         let topAnchor = popupRectY! - bottomSafeArea
-        print(topAnchor)
-        
         let leadingAnchor = popupArrorX! - popupArror.frame.width / 2
         
         popupOption.topAnchor.constraint(equalTo: popupSuperView.topAnchor, constant: topAnchor).isActive = true
@@ -29,16 +27,36 @@ class PreviewOptionPopupViewController: UIViewController {
     }
     
     @IBAction func handlePan(_ gesture: UIPanGestureRecognizer) {
+        let center = popupSuperView.frame.height / 2
+        
         switch gesture.state {
-//        case .began:
-//        case .changed:
+        case .changed:
+            let movement = popupSuperView.center.y + gesture.translation(in: popupOption).y
+            if movement > center {
+                popupSuperView.center.y = popupSuperView.center.y + gesture.translation(in: popupOption).y
+                gesture.setTranslation(CGPoint.zero, in: popupSuperView)
+            }
         case .ended:
-            dismiss(animated: true, completion: nil)
+            if popupSuperView.frame.minY > popupSuperView.frame.height / 10 {
+                dismiss(animated: true, completion: nil)
+            } else {
+                popupSuperView.center.y = center
+            }
         default: break
         }
-        
     }
-    
 }
+
+
+
+// [] 팝업창에 들어가는 옵션들
+// - [] 셀 제거
+// - [] 셀 배경변경
+// - [] 셀 정보
+
+// 만약에 셀 분류가 되면 animatedPreview를 클릭하여 특정 분류만 볼 수 있어야 한다.
+// [] 특정 분류만 보기
+// [] 일시정지
+//
 
 

fce56cb895316f2b3a0714defa7d49bb937dbdae
chanul94 <cksdnfwkd@naver.com>
2021-06-21 01:00:50 +0900

    Implement magic select

--- spriteMaker/spriteMaker/DrawingToolClasses/MagicTool.swift
+++ spriteMaker/spriteMaker/DrawingToolClasses/MagicTool.swift
@@ -11,6 +11,7 @@ class MagicTool {
     var canvas: Canvas!
     var grid: Grid!
     var colorPositions: [Int: [Int]]!
+    var selectedPositions: [Int: [Int]] = [:]
     
     init(_ canvas: Canvas) {
         self.canvas = canvas
@@ -24,7 +25,6 @@ class MagicTool {
         guard let y = pos["y"] else { return }
         let color = canvas.grid.findColorSelected(x: x, y: y)
         colorPositions = grid.getLocations(hex: color)
-        print(colorPositions)
         findSameColorPosition(color, x, y)
     }
     
@@ -33,16 +33,25 @@ class MagicTool {
         return posX.firstIndex(of: y) != nil
     }
     
+    func removePosition(_ x: Int, _ y: Int) {
+        guard let pos = colorPositions[x] else { return }
+        guard let index = pos.firstIndex(of: y) else { return }
+        colorPositions[x]?.remove(at: index)
+    }
+    
+    func addPosition(_ x: Int, _ y: Int) {
+        if (selectedPositions[x] == nil) { selectedPositions[x] = [] }
+        selectedPositions[x]?.append(y)
+    }
+    
     func findSameColorPosition(_ hex: String, _ x: Int, _ y: Int) {
-        if (isPosition(x, y) == false) {
-            // 종료조건
-            return
-        } else {
-            findSameColorPosition(hex, x + 1, y)
-            findSameColorPosition(hex, x - 1, y)
-            findSameColorPosition(hex, x, y + 1)
-            findSameColorPosition(hex, x, y - 1)
-        }
+        // colorPositions의 요소를 하나씩 지워서 만약에 없다면 다음으로 넘어간다.
+        addPosition(x, y)
+        removePosition(x, y)
+        if (isPosition(x + 1, y)) { findSameColorPosition(hex, x + 1, y) }
+        if (isPosition(x - 1, y)) { findSameColorPosition(hex, x - 1, y) }
+        if (isPosition(x, y + 1)) { findSameColorPosition(hex, x, y + 1) }
+        if (isPosition(x, y - 1)) { findSameColorPosition(hex, x, y - 1) }
     }
     
 }

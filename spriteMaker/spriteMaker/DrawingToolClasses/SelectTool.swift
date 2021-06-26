0fd3a33fdcf9460f35b9d2e0f3ffd1830acbb9b1
chanul94 <cksdnfwkd@naver.com>
2021-06-26 23:58:34 +0900

    Add functions at selectTool

--- spriteMaker/spriteMaker/DrawingToolClasses/SelectTool.swift
+++ spriteMaker/spriteMaker/DrawingToolClasses/SelectTool.swift
@@ -11,9 +11,9 @@ class SelectTool {
     var canvas: Canvas!
     var grid: Grid!
     var pixelLen: CGFloat!
-    var selectedPositions: [String: [Int: [Int]]] = [:]
     var outlineTerm: CGFloat!
     var outlineToggle: Bool!
+    var selectedPositions: [String: [Int: [Int]]] = [:]
     var drawOutlineInterval: Timer?
 
     init(_ canvas: Canvas) {
@@ -23,5 +23,53 @@ class SelectTool {
         self.outlineTerm = self.pixelLen / 4
     }
     
+    func isSelectedPosition(_ hex: String, _ x: Int, _ y: Int) -> Bool {
+        guard let posHex = selectedPositions[hex] else { return false }
+        guard let posX = posHex[x] else { return false }
+        return posX.firstIndex(of: y) != nil
+    }
     
+    func addPosition(_ hex: String, _ x: Int, _ y: Int) {
+        if (selectedPositions[hex] == nil) { selectedPositions[hex] = [:] }
+        if (selectedPositions[hex]?[x] == nil) { selectedPositions[hex]?[x] = [] }
+        selectedPositions[hex]?[x]?.append(y)
+    }
+    
+    func drawHorizontalOutline(_ context: CGContext, _ x: CGFloat, _ y: CGFloat, _ toggle: Bool!) {
+        if (toggle) {
+            context.move(to: CGPoint(x: x, y: y))
+            context.addLine(to: CGPoint(x: x + outlineTerm, y: y))
+            context.move(to: CGPoint(x: x + (outlineTerm * 3), y: y))
+            context.addLine(to: CGPoint(x: x + (outlineTerm * 4), y: y))
+        } else {
+            context.move(to: CGPoint(x: x + outlineTerm, y: y))
+            context.addLine(to: CGPoint(x: x + (outlineTerm * 3), y: y))
+        }
+        context.strokePath()
+    }
+    
+    func drawVerticalOutline(_ context: CGContext, _ x: CGFloat, _ y: CGFloat, _ toggle: Bool!) {
+        if (toggle) {
+            context.move(to: CGPoint(x: x, y: y))
+            context.addLine(to: CGPoint(x: x, y: y + outlineTerm))
+            context.move(to: CGPoint(x: x, y: y + (outlineTerm * 3)))
+            context.addLine(to: CGPoint(x: x, y: y + (outlineTerm * 4)))
+        } else {
+            context.move(to: CGPoint(x: x, y: y + outlineTerm))
+            context.addLine(to: CGPoint(x: x, y: y + (outlineTerm * 3)))
+        }
+        context.strokePath()
+    }
+    
+    func startDrawOutlineInterval() {
+        outlineToggle = true
+        drawOutlineInterval = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true)
+        { (Timer) in
+            if (self.canvas.panelVC.drawingToolVM.selectedTool.name != "Magic") {
+                Timer.invalidate()
+            }
+            self.canvas.setNeedsDisplay()
+            self.outlineToggle = !self.outlineToggle
+        }
+    }
 }

58e52280ad23edc8479debdcf961a2cbe6447be9
chanul94 <cksdnfwkd@naver.com>
2021-08-24 23:03:59 +0900

    Implement ExportControllerView layout

--- spriteMaker/spriteMaker/ExportViewController.swift
+++ spriteMaker/spriteMaker/ExportViewController.swift
@@ -13,23 +13,47 @@ import AssetsLibrary
 
 class ExportViewController: UIViewController {
     @IBOutlet weak var backgroundView: UIView!
-    @IBOutlet weak var closeHandle: UIView!
+    @IBOutlet weak var pngBtn: UIButton!
+    @IBOutlet weak var gifBtn: UIView!
+    @IBOutlet weak var clearBtn: UIButton!
+    @IBOutlet weak var speedPickerView: UIPickerView!
+    @IBOutlet weak var selectionPanelCV: UICollectionView!
+    
     var superViewController: ViewController!
+    var framePanelCVC: FramePanelCVC!
+    var categoryPanelCVC: CategoryPanelCVC!
+    var frameData: [Frame]!
+    var categoryData: [String]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setSideCorner(target: backgroundView, side: "top", radius: backgroundView.bounds.width / 25)
-        setSideCorner(target: closeHandle, side: "all", radius: closeHandle.frame.height / 2)
+        setSideCorner(target: backgroundView, side: "top", radius: backgroundView.frame.width / 25)
+        setSideCorner(target: pngBtn, side: "all", radius: pngBtn.frame.height / 4)
+        setSideCorner(target: gifBtn, side: "all", radius: gifBtn.frame.height / 4)
+        setSideCorner(target: clearBtn, side: "all", radius: clearBtn.frame.height / 4)
+        setViewShadow(target: pngBtn, radius: 3, opacity: 0.3)
+        setViewShadow(target: gifBtn, radius: 3, opacity: 0.3)
+        setViewShadow(target: clearBtn, radius: 3, opacity: 0.3)
+        
+        speedPickerView.selectRow(1, inComponent: 0, animated: true)
+        guard let time = superViewController.timeMachineVM.presentTime else { return }
+        frameData = time.frames
+        categoryData = []
+        for frame in frameData {
+            if categoryData.last == frame.category {
+                // 중복된 수를 카운트
+                categoryData[categoryData.count - 2] = String(Int(categoryData[categoryData.count - 2])! + 1)
+            } else {
+                categoryData.append("0")
+                categoryData.append(frame.category)
+            }
+        }
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         // start animated image
         let view = superViewController.panelContainerViewController.previewImageToolBar.animatedPreview!
         view.startAnimating()
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        guard let destination = segue.destination as? ExportPanelViewController else { return }
     }
     
     func animateImages(_ data: Time?, targetImageView: UIImageView) {
@@ -42,10 +66,6 @@ class ExportViewController: UIViewController {
         targetImageView.animationImages = images
         targetImageView.animationDuration = TimeInterval(images.count)
         targetImageView.startAnimating()
-    }
-    
-    @IBAction func tappedCloseBtn(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
     }
     
     @IBAction func tappedBackground(_ sender: Any) {
@@ -73,7 +93,7 @@ class ExportViewController: UIViewController {
         let path = documentsDirectoryPath.appending(filename)
         let cfURL = URL(fileURLWithPath: path) as CFURL
         
-        let fileProperties = [kCGImagePropertyGIFDictionary as String: [kCGImagePropertyGIFLoopCount as String: 0]]
+        let fileProperties = [kCGImagePropertyGIFDictionary as String: [kCGImagePropertyGIFLoopCount as String: 2]]
         let gifProperties = [kCGImagePropertyGIFDictionary as String: [kCGImagePropertyGIFDelayTime as String: 0.125]]
         
         if let destination = CGImageDestinationCreateWithURL(cfURL, kUTTypeGIF, photos.count, nil) {
@@ -115,52 +135,157 @@ class ExportViewController: UIViewController {
     }
 }
 
-class ExportPanelViewController: UIViewController {
-    var oneSideLength: CGFloat!
+extension ExportViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
     
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 10
+    }
 }
 
-extension ExportPanelViewController: UICollectionViewDataSource {
+extension ExportViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if (row == 0) {
+            return "Speed"
+        }
+        return String(row)
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        var pickerLabel = view as? UILabel
+
+        if (pickerLabel == nil)
+        {
+            pickerLabel = UILabel()
+            pickerLabel?.font = UIFont(name: "System", size: 16)
+            pickerLabel?.textAlignment = .center
+        }
+        pickerLabel?.text = row == 0 ? "Speed" : String(row)
+        return pickerLabel!
+    }
+}
+
+extension ExportViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        return 2
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         switch indexPath.row {
         case 0:
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "pngExportCell", for: indexPath) as! pngExportCell
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FramePanelCVC", for: indexPath) as! FramePanelCVC
+            framePanelCVC = cell
+            cell.superCollectionView = self
+            cell.frames = frameData
+            setViewShadow(target: cell, radius: 5, opacity: 0.3)
             return cell
         case 1:
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "gifExportCell", for: indexPath) as! gifExportCell
-            return cell
-        case 2:
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "spriteExportCell", for: indexPath) as! spriteExportCell
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CategoryPanelCVC", for: indexPath) as! CategoryPanelCVC
+            categoryPanelCVC = cell
+            cell.superCollectionView = self
+            cell.categorys = categoryData
+            cell.frameOneSideLen = ((selectionPanelCV.frame.height - 65) / 3 * 2) - 5
             return cell
         default:
             return UICollectionViewCell()
         }
     }
-    
-    
 }
 
-extension ExportPanelViewController: UICollectionViewDelegateFlowLayout {
+extension ExportViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let len = oneSideLength!
-        return CGSize(width: len, height: len - 10)
+        if (indexPath.row == 0) {
+            return CGSize(
+                width: selectionPanelCV.frame.width,
+                height: (selectionPanelCV.frame.height - 65) / 3 * 2
+            )
+        } else {
+            return CGSize(
+                width: selectionPanelCV.frame.width,
+                height: (selectionPanelCV.frame.height - 65) / 3
+            )
+        }
     }
 }
 
-class pngExportCell: UICollectionViewCell {
+class FramePanelCVC: UICollectionViewCell {
+    @IBOutlet weak var frameCV: UICollectionView!
+    var superCollectionView: ExportViewController!
+    var frames: [Frame]!
     
 }
 
-class gifExportCell: UICollectionViewCell {
+extension FramePanelCVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return frames.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FrameItemCVC", for: indexPath) as! FrameItemCVC
+        cell.frameImage.image = frames[indexPath.row].renderedImage
+        return cell
+    }
+}
+
+extension FramePanelCVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.frame.height - 5, height: self.frame.height - 5)
+    }
+}
+
+extension FramePanelCVC: UICollectionViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        superCollectionView.categoryPanelCVC.categoryCV.contentOffset.x = scrollView.contentOffset.x
+    }
+}
+
+class FrameItemCVC: UICollectionViewCell {
+    @IBOutlet weak var frameImage: UIImageView!
     
 }
 
-class spriteExportCell: UICollectionViewCell {
+
+
+class CategoryPanelCVC: UICollectionViewCell {
+    @IBOutlet weak var categoryCV: UICollectionView!
+    var superCollectionView: ExportViewController!
+    var categorys: [String]!
+    var categoryVM = CategoryListViewModel()
+    var frameOneSideLen: CGFloat!
     
 }
 
+extension CategoryPanelCVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return categorys.count / 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CategoryItemCVC", for: indexPath) as! CategoryItemCVC
+        let category = categorys[(indexPath.row * 2) + 1]
+        cell.categoryLabel.text = category
+        cell.backgroundColor = categoryVM.getCategoryColor(category: category)
+        setSideCorner(target: cell, side: "all", radius: cell.frame.height / 2)
+        return cell
+    }
+}
 
+extension CategoryPanelCVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = frameOneSideLen! + ((frameOneSideLen! + 5) * CGFloat(Int(categorys[indexPath.row * 2])!))
+        return CGSize(width: width, height: self.frame.height - 10)
+    }
+}
+
+extension CategoryPanelCVC: UICollectionViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        superCollectionView.framePanelCVC.frameCV.contentOffset.x = scrollView.contentOffset.x
+    }
+}
+
+class CategoryItemCVC: UICollectionViewCell {
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+}

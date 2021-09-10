95c00eafa5a5c6f6e11dae405fcc424e9a3ba836
chanul94 <cksdnfwkd@naver.com>
2021-09-10 18:05:22 +0900

    Implement gameStickView

--- spriteMaker/spriteMaker/TestViewController.swift
+++ spriteMaker/spriteMaker/TestViewController.swift
@@ -13,30 +13,81 @@ class TestViewController: UIViewController {
     @IBOutlet weak var tabBarView: UIView!
     @IBOutlet weak var pickerView: UIView!
     @IBOutlet weak var toggleWidthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var gameStickView: GameStick!
+    @IBOutlet weak var gameStickImageView: UIImageView!
     
     var items = ["Game", "Message", "AppleWatch"]
+    let gameCommands = ["up", "down", "left", "right"]
     
     override func viewDidLoad() {
-        print(pickerView.frame)
         segmentedControl.selectedSegmentIndex = 0
-        
         setSideCorner(target: tabBarView, side: "top", radius: tabBarView.bounds.width / 25)
-        print(pickerView.frame)
-        let picker = UIPickerView()
         
+        // set picker
+        let picker = UIPickerView()
         picker.dataSource = self
         picker.delegate = self
-        
         picker.transform = CGAffineTransform(rotationAngle: -90 * (.pi/180))
         picker.frame = pickerView.bounds
         picker.center = pickerView.center
-        
         pickerView.addSubview(picker)
+        
+        // set gameStick
+        gameStickView.testViewController = self
     }
     
     @IBAction func tappedButton(_ sender: Any) {
         dismiss(animated: false, completion: nil)
     }
+    
+}
+
+import AudioToolbox
+
+
+class GameStick: UIView {
+    weak var testViewController: TestViewController!
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let pos = touches.first?.location(in: self) else { return }
+        guard let key = calcTouchPosition(pos) else { return }
+        
+        changeGameStickViewImage(key)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let pos = touches.first?.location(in: self) else { return }
+        guard let key = calcTouchPosition(pos) else {
+            initGameStickViewImage()
+            return
+        }
+        changeGameStickViewImage(key)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        initGameStickViewImage()
+    }
+    
+    func initGameStickViewImage() {
+        testViewController.gameStickImageView.image = UIImage(systemName: "circle.grid.cross")
+    }
+    
+    func changeGameStickViewImage(_ keyIndex: Int) {
+        let keyCommand = testViewController.gameCommands[keyIndex]
+        testViewController.gameStickImageView.image = UIImage(systemName: "circle.grid.cross.\(keyCommand).fill")
+    }
+    
+    func calcTouchPosition(_ location: CGPoint) -> Int? {
+        let calc_a = location.x > location.y
+        let calc_b = self.frame.width - location.x > location.y
+
+        if (calc_a && calc_b) { return 0 }
+        else if (!calc_a && !calc_b) { return 1 }
+        else if (!calc_a && calc_b) { return 2 }
+        else if (calc_a && !calc_b) { return 3 }
+        return nil
+    }
+    
 }
 
 extension TestViewController: UIPickerViewDataSource, UIPickerViewDelegate {

e65018017e0e025cf0f95513b450fa6c9a1d7919
chanul94 <cksdnfwkd@naver.com>
2021-09-10 18:11:04 +0900

    Remove other apps

--- myFirebase/Pods/Target Support Files/Pods-myFirebase/Pods-myFirebase-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_myFirebase : NSObject
-@end
-@implementation PodsDummy_Pods_myFirebase
-@end
--- nextNetflix/Pods/Target Support Files/Pods-nextNetflix/Pods-nextNetflix-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_nextNetflix : NSObject
-@end
-@implementation PodsDummy_Pods_nextNetflix
-@end
